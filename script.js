2f97407cfe310b77f1240fb9b240a87110587337
sky <sorasymiku@gmail.com>
2025-02-21 22:37:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function resizeCanvas() {
         canvas.width = 700; // サイズを少し小さく
         canvas.height = 500; // サイズを少し小さく
     } else {
-        canvas.width = window.innerWidth * 0.6;
+        canvas.width = window.innerWidth * 0.5;
         canvas.height = window.innerHeight * 0.4; // 少し小さく
     }
 }
